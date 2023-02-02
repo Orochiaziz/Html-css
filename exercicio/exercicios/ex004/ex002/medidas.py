6626b33dba212911065f1e57f9c4d6d9abd018bf
Orochiaziz <124267123+Orochiaziz@users.noreply.github.com>
2023-02-02 13:12:55 +0000

    Initial commit

---
+++ exercicio/exercicios/ex004/ex002/medidas.py
@@ -0,0 +1,14 @@
+medida = float(input('Uma distancia em metro '))
+km = medida / 1000
+hm = medida / 100
+dam =  medida / 10
+dm = medida * 10
+cm = medida * 100
+mm = medida * 1000
+print('A medida em {} metro e igual a {} KM'.format(medida, km))
+print('A medida em {} metro e iguel a {} hm '.format(medida, hm))
+print('A medida em {} metro e igual a {} dam '.format(medida, dam))
+print('A medida em {} metro e igual a {} dm '.format(medida, dm))
+print('A medida em {} metro e igual a {} cm '.format(medida, cm))
+print('A medida em {} metro e igual a {} mm e'.format(medida, mm,))
+
---
+++ exercicio/exercicios/ex009/jvScrip.js
@@ -0,0 +1,2 @@
+window.alert('Este campo e so para os exclusivos')
+var n1 = window.prompt(number('Degite o seu primeiro valor: '))
